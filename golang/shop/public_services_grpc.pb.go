fcabab8675260ce0535f931ce2352e5df740b5aa
Shubamakabra <magnus.engoe@hotmail.no>
2023-01-17 11:47:34 +0100

    feat: Implemented customer, order and communication proto files

--- golang/shop/public_services_grpc.pb.go
+++ golang/shop/public_services_grpc.pb.go
@@ -2,9 +2,9 @@
 // versions:
 // - protoc-gen-go-grpc v1.2.0
 // - protoc             v3.21.12
-// source: proto/payment/public_services.proto
+// source: proto/shop/public_services.proto
 
-package payment
+package shop
 
 import (
 	context "context"
@@ -35,7 +35,7 @@ func NewCustomerClient(cc grpc.ClientConnInterface) CustomerClient {
 
 func (c *customerClient) GetCustomer(ctx context.Context, in *GetCustomerRequest, opts ...grpc.CallOption) (*GetCustomerResponse, error) {
 	out := new(GetCustomerResponse)
-	err := c.cc.Invoke(ctx, "/payment.public.Customer/GetCustomer", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/shop.public.customer/GetCustomer", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -80,7 +80,7 @@ func _Customer_GetCustomer_Handler(srv interface{}, ctx context.Context, dec fun
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/payment.public.Customer/GetCustomer",
+		FullMethod: "/shop.public.customer/GetCustomer",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
 		return srv.(CustomerServer).GetCustomer(ctx, req.(*GetCustomerRequest))
@@ -92,7 +92,7 @@ func _Customer_GetCustomer_Handler(srv interface{}, ctx context.Context, dec fun
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
 var Customer_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "payment.public.Customer",
+	ServiceName: "shop.public.customer",
 	HandlerType: (*CustomerServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
@@ -101,91 +101,5 @@ var Customer_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "proto/payment/public_services.proto",
-}
-
-// OrderClient is the client API for Order service.
-//
-// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type OrderClient interface {
-	GetOrder(ctx context.Context, in *GetOrderRequest, opts ...grpc.CallOption) (*GetOrderResponse, error)
-}
-
-type orderClient struct {
-	cc grpc.ClientConnInterface
-}
-
-func NewOrderClient(cc grpc.ClientConnInterface) OrderClient {
-	return &orderClient{cc}
-}
-
-func (c *orderClient) GetOrder(ctx context.Context, in *GetOrderRequest, opts ...grpc.CallOption) (*GetOrderResponse, error) {
-	out := new(GetOrderResponse)
-	err := c.cc.Invoke(ctx, "/payment.public.Order/GetOrder", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-// OrderServer is the server API for Order service.
-// All implementations must embed UnimplementedOrderServer
-// for forward compatibility
-type OrderServer interface {
-	GetOrder(context.Context, *GetOrderRequest) (*GetOrderResponse, error)
-	mustEmbedUnimplementedOrderServer()
-}
-
-// UnimplementedOrderServer must be embedded to have forward compatible implementations.
-type UnimplementedOrderServer struct {
-}
-
-func (UnimplementedOrderServer) GetOrder(context.Context, *GetOrderRequest) (*GetOrderResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetOrder not implemented")
-}
-func (UnimplementedOrderServer) mustEmbedUnimplementedOrderServer() {}
-
-// UnsafeOrderServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to OrderServer will
-// result in compilation errors.
-type UnsafeOrderServer interface {
-	mustEmbedUnimplementedOrderServer()
-}
-
-func RegisterOrderServer(s grpc.ServiceRegistrar, srv OrderServer) {
-	s.RegisterService(&Order_ServiceDesc, srv)
-}
-
-func _Order_GetOrder_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(GetOrderRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(OrderServer).GetOrder(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/payment.public.Order/GetOrder",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(OrderServer).GetOrder(ctx, req.(*GetOrderRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-// Order_ServiceDesc is the grpc.ServiceDesc for Order service.
-// It's only intended for direct use with grpc.RegisterService,
-// and not to be introspected or modified (even as a copy)
-var Order_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "payment.public.Order",
-	HandlerType: (*OrderServer)(nil),
-	Methods: []grpc.MethodDesc{
-		{
-			MethodName: "GetOrder",
-			Handler:    _Order_GetOrder_Handler,
-		},
-	},
-	Streams:  []grpc.StreamDesc{},
-	Metadata: "proto/payment/public_services.proto",
+	Metadata: "proto/shop/public_services.proto",
 }
